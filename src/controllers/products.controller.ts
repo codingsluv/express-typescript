c0497495c13781c651603f3161994826e85fd390
MuhammadNur <delusionboys974@gmail.com>
2025-01-19 12:21:22 +0700

    feat(products): implement create and get all products endpoints
    - Added new endpoints to create and get all products.
    - Implemented controllers and routes for the new endpoints.
    - Implemented input validation for the create product endpoint.

---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import { inputProductsValidation } from "../validation/products.validation";
+
+
+export const getAllProducts = (req:Request, res:Response, next:NextFunction):any => {
+    try {
+        res.status(200).json({ message: "Product list was successfully", products: [
+            {
+                id: 1,
+                nameProduct: "Iphone 14",
+                price: 10000,
+                stock: 10,
+            },
+            {
+                id: 2,
+                nameProduct: "Iphone 14 Pro",
+                price: 12000,
+                stock: 10,  
+            },
+            {
+                id: 3,
+                nameProduct: "Iphone 14 Pro Max",
+                price: 15000,   
+                stock: 10,
+            }
+        ] })
+    } catch (error:Error | any) {
+        next(new Error("Error occured while getting all products :" + error.message))
+    }
+}
+
+export const createProduct = (req:Request, res:Response, next:NextFunction):any => {
+    const { error, value } = inputProductsValidation(req.body);
+    if(error != null) {
+        res.status(400).json({
+            message: "Validation error",
+            error: error.details[0].message,
+            data: value
+        })
+    }
+    else {
+        res.status(200).json({
+            message: "Product created was successfully",
+            error: null,
+            data: value
+        })
+    }
+}
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,42 +1,14 @@
-import { Request, Response, Router } from "express";
-import { inputProductsValidation } from "../validation/products.validation";
+import { NextFunction, Request, Response, Router } from "express";
+import { createProduct, getAllProducts } from "../controllers/products.controller";
 
 const productRouter:Router = Router();
 
-productRouter.get("/products", (req:Request, res:Response) => {
-    res.status(200).json({
-        message: "Product list was successfully",
-        products: [
-            {
-                id: 1,
-                nameProduct: "Iphone 14",
-                price: 10000,
-            },
-            {
-                id: 2,
-                nameProduct: "Iphone 14 Pro",
-                price: 12000,
-            }
-        ]
-    })
+productRouter.get("/products", (req:Request, res:Response, next:NextFunction) => {
+    getAllProducts(req, res, next)
 })
 
-productRouter.post("/products", (req:Request, res:Response) => {
-    const { error, value } = inputProductsValidation(req.body);
-    if(error != null) {
-        res.status(400).json({
-            message: "Validation error",
-            error: error.details[0].message,
-            data: value
-        })
-    }
-    else {
-        res.status(200).json({
-            message: "Product was successfully",
-            error: null,
-            data: value
-        })
-    }
+productRouter.post("/products", (req:Request, res:Response, next:NextFunction) => {
+    createProduct(req, res, next)
 })
 
 export default productRouter;
