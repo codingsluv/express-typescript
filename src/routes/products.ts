33746e2cb2e7ed2742b9f6f0913fb4413534794f
MuhammadNur <delusionboys974@gmail.com>
2025-01-19 11:59:55 +0700

    feat(validation): implemented product validation
    Added Joi for product validation to improve the robustness and reliability of the product creation process. This ensures that the data entered is valid, consistent, and meets the required criteria.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { inputProductsValidation } from "../validation/products.validation";
 
 const productRouter:Router = Router();
 
@@ -18,6 +19,24 @@ productRouter.get("/products", (req:Request, res:Response) => {
             }
         ]
     })
+})
+
+productRouter.post("/products", (req:Request, res:Response) => {
+    const { error, value } = inputProductsValidation(req.body);
+    if(error != null) {
+        res.status(400).json({
+            message: "Validation error",
+            error: error.details[0].message,
+            data: value
+        })
+    }
+    else {
+        res.status(200).json({
+            message: "Product was successfully",
+            error: null,
+            data: value
+        })
+    }
 })
 
 export default productRouter;
